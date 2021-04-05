04ac33d691ed707e1c2dcd359d08ee0e58037674
TheKatze <leofel.katz@gmail.com>
2021-04-05 14:28:31 +0200

    Add about page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <v-app>
+    <v-navigation-drawer app v-model="navDrawer">
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content> App </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content> About </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app dark elevate-on-scroll>
+      <v-app-bar-nav-icon @click="navDrawer = true" />
       <h2>pwdhash</h2>
 
       <v-spacer />
-      <v-btn @click="setPassword('')" v-if="isUnlocked">
-        Lock
-      </v-btn>
-      <v-btn color="primary" @click="$refs.dialog.open()" v-else>
-        Unlock
-      </v-btn>
+      <span v-if="$route.path === '/'">
+        <v-btn @click="setPassword('')" v-if="isUnlocked">
+          Lock
+        </v-btn>
+        <v-btn color="primary" @click="$refs.dialog.open()" v-else>
+          Unlock
+        </v-btn>
+      </span>
     </v-app-bar>
     <v-main>
       <router-view />
@@ -39,6 +58,8 @@ export default class App extends Vue {
 
   @main.Mutation
   public setPassword!: MutationMethod;
+
+  private navDrawer = false;
 
   created() {
     this.$vuetify.theme.dark = true;
